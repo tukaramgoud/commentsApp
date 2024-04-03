cd24ec2247a67f61d1992378fc3016dc0fd2e894
tukaramgoud <tukaramgoud2804.com>
2024-04-03 06:08:25 +0000

    adding designing HTML

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,48 @@
 // Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {itemDetails} = props
+  return (
+    <div className="item-container">
+      <div className="comments-container">
+        <div className="person-comment-container">
+          <p className="round-name">s</p>
+          <div className="description-container">
+            <div className="name-container">
+              <h1 className="person-name">Ravi Kiran Rathod</h1>
+              <p className="time-styling">2 minutes ago</p>
+            </div>
+            <p className="description-para">
+              some random text that is shown on the display of the web
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="like-dislike-container">
+        <div className="like-container">
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+              alt="disLike"
+              className="image-sizing"
+            />
+            <button type="button" className="button">
+              Like
+            </button>
+          </div>
+        </div>
+        <button type="button" className="button-new">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+            className="image-sizing"
+          />
+        </button>
+      </div>
+      <hr className="line" />
+    </div>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import CommentItem from '../CommentItem'
 import './index.css'
 
 const initialContainerBackgroundClassNames = [
@@ -16,44 +17,46 @@ class Comments extends Component {
   render() {
     console.log(initialContainerBackgroundClassNames)
     return (
-      <div className="background-container">
-        <h1 className="comments-heading">Comments</h1>
-        <div className="userInput-image-container">
-          <form className="user-input-container">
-            <label htmlFor="note" className="para">
-              say something about our 4.0 Technologies
-            </label>
-            <input
-              type="search"
-              className="user-input"
-              placeholder="Your Name"
-              id="note"
-            />
-            <textarea
-              rows="10"
-              cols="52"
-              className="text-area-input"
-              placeholder="Your Comment"
-            />
-            <button type="submit" className="button">
-              Add Comment
-            </button>
-          </form>
-          <div className="image-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
-              alt="comments"
-            />
+      <>
+        <div className="background-container">
+          <h1 className="comments-heading">Comments</h1>
+          <div className="userInput-image-container">
+            <form className="user-input-container">
+              <label htmlFor="note" className="para">
+                say something about our 4.0 Technologies
+              </label>
+              <input
+                type="search"
+                className="user-input"
+                placeholder="Your Name"
+                id="note"
+              />
+              <textarea
+                rows="10"
+                cols="52"
+                className="text-area-input"
+                placeholder="Your Comment"
+              />
+              <button type="submit" className="button">
+                Add Comment
+              </button>
+            </form>
+            <div className="image-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+                alt="comments"
+              />
+            </div>
           </div>
-        </div>
-        <hr className="line" />
-        <div className="comments-container">
+          <hr className="line" />
+
           <div className="count-of-comments">
             <p className="count-para">0</p>
             <p className="count-comment-para">Comments</p>
           </div>
         </div>
-      </div>
+        <CommentItem />
+      </>
     )
   }
 }
